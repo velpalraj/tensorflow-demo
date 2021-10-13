878c3ddc68ffa5ae81f5a9638ee8a76dce870df9
velpalraj <64446138+velpalraj@users.noreply.github.com>
2021-10-13 11:34:05 +0530

    Update upload_data.py

--- upload_data.py
+++ upload_data.py
@@ -5,7 +5,7 @@ s3_resource = boto3.resource('s3')
 
 BUCKET_NAME_SOURCE = os.environ.get('SRC_BKT_NAME')
 BUCKET_NAME_OUTPUT = os.environ.get('OUTPUT_BKT')
-PREFIX = 'input/data/'
+PREFIX = 'input/data/training/'
 
 s3_resource.Bucket(BUCKET_NAME_SOURCE).upload_file("data/abalone_train.csv", PREFIX + 'abalone_train.csv')
 s3_resource.Bucket(BUCKET_NAME_SOURCE).upload_file("data/abalone_train.csv", PREFIX + 'abalone_predict.csv')
